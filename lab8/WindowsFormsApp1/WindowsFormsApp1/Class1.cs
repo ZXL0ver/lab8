394814435e018200dee1c2490dde97dd42055e44
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:19:23 +0300

    Add files via upload

---
+++ lab8/WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+interface ICircle
+{
+    void Draw(Graphics g);
+}
+
+// The concrete Circle class
+class Circle : ICircle
+{
+    private int _x, _y, _radius;
+
+    public Circle(int x, int y, int radius)
+    {
+        _x = x;
+        _y = y;
+        _radius = radius;
+    }
+
+    public void Draw(Graphics g)
+    {
+        g.DrawEllipse(new Pen(Color.Black), _x - _radius, _y - _radius, 2 * _radius, 2 * _radius);
+    }
+}
+
+// The abstract decorator class
+abstract class CircleDecorator : ICircle
+{
+    private ICircle _circle;
+
+    public CircleDecorator(ICircle circle)
+    {
+        _circle = circle;
+    }
+
+    public virtual void Draw(Graphics g)
+    {
+        _circle.Draw(g);
+    }
+}
+
+// The ConsoleDecorator class
+class ConsoleDecorator : CircleDecorator
+{
+    public ConsoleDecorator(ICircle circle) : base(circle)
+    {
+    }
+
+    public override void Draw(Graphics g)
+    {
+        base.Draw(g);
+        Console.WriteLine("Drawing Circle on Console");
+    }
+}
+
+// The VisualDecorator class
+class VisualDecorator : CircleDecorator
+{
+    private Form _form;
+
+    public VisualDecorator(ICircle circle, Form form) : base(circle)
+    {
+        _form = form;
+    }
+
+    public override void Draw(Graphics g)
+    {
+        base.Draw(g);
+        g.Dispose(); // Clean up the graphics object
+
+        // Draw the circle on the form
+        using (Graphics formGraphics = _form.CreateGraphics())
+        {
+            base.Draw(formGraphics);
+        }
+
+        Console.WriteLine("Drawing Circle on Visual Layout");
+    }
+}
---
+++ lab8/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private ICircle _circle;
+        public Form1()
+        {
+            InitializeComponent();
+            _circle = new Circle(50, 50, 25);
+            _circle = new ConsoleDecorator(_circle);
+            _circle = new VisualDecorator(_circle, this);
+
+            this.Paint += Client_Paint;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Client_Paint(object sender, PaintEventArgs e)
+        {
+            _circle.Draw(e.Graphics);
+        }
+    }
+}
